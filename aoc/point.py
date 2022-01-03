c267baec9d7daa2afc3741e5c6193b5a3be1d8a5
spossner <github@possner.de>
2022-01-03 11:24:02 +0100

    add adjacent generator

--- aoc/point.py
+++ aoc/point.py
@@ -7,11 +7,11 @@ Point3d = namedtuple('Point3d', 'x,y,z', defaults=[0, 0, 0])
 DIRECT_ADJACENTS = ((0, -1), (-1, 0), (1, 0), (0, 1))  # 4 adjacent nodes
 ALL_ADJACENTS = ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1))
 
-
 NORTH = Point(0, -1)
 EAST = Point(1, 0)
 SOUTH = Point(0, 1)
 WEST = Point(-1, 0)
+
 
 def translate(p: Union[Point, Point3d, tuple], offset: Union[Point, Point3d, tuple]) -> Union[Point, Point3d, tuple]:
     if type(p) == Point:
@@ -22,7 +22,7 @@ def translate(p: Union[Point, Point3d, tuple], offset: Union[Point, Point3d, tup
         assert len(p) == len(offset)
         result = []
         for i in range(len(p)):
-            result.append(p[i]+offset[i])
+            result.append(p[i] + offset[i])
         return tuple(result)
     raise ValueError(f'can not translate {type(p)}')
 
@@ -37,3 +37,21 @@ def rot_ccw(p: tuple) -> tuple:
 
 def manhattan_distance(p) -> int:
     return sum(map(abs, p))
+
+
+def all_adjacent_iter(p: tuple, width: int = 0, height: int = 0):
+    yield from _adjacent_iter(p, width, height)
+
+
+def direct_adjacent_iter(p: tuple, width: int = 0, height: int = 0):
+    yield from _adjacent_iter(p, width, height, DIRECT_ADJACENTS)
+
+
+def _adjacent_iter(p: tuple, width: int = 0, height: int = 0, adjacents=ALL_ADJACENTS):
+    for dx, dy in adjacents:
+        np = Point(p[0] + dx, p[1] + dy)
+        if width > 0 and (np.x < 0 or np.x >= width):
+            continue
+        if height > 0 and (np.y < 0 or np.y >= height):
+            continue
+        yield np
--- tests/test.py
+++ tests/test.py
@@ -10,3 +10,11 @@ if __name__ == '__main__':
     assert manhattan_distance(Point(-2, -4)) == 6
 
     assert extract_ints("Set value to 2 and jump -34.") == [2, -34]
+
+    # (-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)
+    p = list(all_adjacent_iter(Point(12,5)))
+    assert p == [(11, 4), (12, 4), (13, 4), (11, 5), (13, 5), (11, 6), (12, 6), (13, 6)]
+
+    p = list(direct_adjacent_iter(Point(12, 5)))
+    print(p)
+    assert p == [(12, 4), (11, 5), (13, 5), (12, 6),]
