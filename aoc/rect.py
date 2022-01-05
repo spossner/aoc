55f24ce4f1cc64a35115c1ada30abe841d42140c
spossner <github@possner.de>
2022-01-05 09:17:44 +0100

    Rect now with default values (all 0) and some methods to change the rect (create boundary rect of multiple given points, extend rect to include a given point, grow in all dimensions by a given value)

--- aoc/rect.py
+++ aoc/rect.py
@@ -11,18 +11,70 @@ class Rect:
     """
     Rect class (x,y) with width and height all inclusive
     """
-    x: int
-    y: int
-    w: int
-    h: int
+    x: int = 0
+    y: int = 0
+    w: int = 0
+    h: int = 0
+
+    @classmethod
+    def boundary(cls, points):
+        boundary = None
+        for p in points:
+            if boundary is None:
+                boundary = Rect(*p, 1, 1)
+            else:
+                boundary.extend(*p)
+        return boundary
+
+    def grow(self, i):
+        """
+        Grows this rectangle by i in all dimensions (x-i,y-i,w+2*i,h+2*i).
+        Note that negative amount will shrink the rect.
+        :param i: Amount to grow in all dimensions
+        :return: the rect itself
+        :raises ValueError: if the resulting rect would have negative width or height
+        """
+        new_width = self.w + (i << 1)
+        new_height = self.h + (i << 1)
+        if new_width < 0 or new_height < 0:
+            raise ValueError(f'shrinked rectangle below zero ({new_width}, {new_height})')
+
+        self.x -= i
+        self.y -= i
+        self.w = new_width
+        self.h = new_height
+        return self
+
+    def extend(self, x, y):
+        """
+        Extend the rect to also contain the given point
+        :param x: the x coordinate to also include
+        :param y: the y coordinate to also include
+        :return: the rect itself for further concatenation
+        """
+        if x < self.x:
+            self.w = self.w + self.x - x
+            self.x = x
+        elif x > self.x + self.w:
+            self.w = x - self.x + 1
+        if y < self.y:
+            self.h = self.h + self.y - y
+            self.y = y
+        elif y > self.y + self.h:
+            self.h = y - self.y + 1
+        return self
 
     def contains_point(self, x, y):
         return self.x <= x < self.x + self.w and self.y <= y < self.y + self.h
 
     def points(self):
+        """
+        Generator presenting all points in the rect starting top left (x,y) and proceeding row by row (e.g. (x+1,y) as next point)
+        :return: Point in rect (generator)
+        """
         for y in range(self.y, self.y + self.h):
             for x in range(self.x, self.x + self.w):
-                yield (x, y)
+                yield Point(x, y)
 
     def contains(self, other):
         return self.x <= other.x and self.y <= other.y and self.x + self.w >= other.x + other.w and self.y + self.h >= other.y + other.h and self.x + self.w > other.x and self.y + self.h > other.y
--- tests/test_rect.py
+++ tests/test_rect.py
@@ -1,16 +1,38 @@
+from itertools import chain
+
 from aoc import Rect, Point
+
 
 def test_rect():
     r1 = Rect(5, 5, 30, 30)
     r2 = Rect(15, 15, 30, 30)
-    r3 = Rect(15,15,30,30)
+    r3 = Rect(15, 15, 30, 30)
     assert r1 != r2
     assert r2 == r3
     assert r1.contains(r2) is False
     assert r2.contains(r3) is True
-    assert r1.contains_point(5,5)
-    assert r1.contains_point(34,34)
-    assert r1.contains_point(35,35) is False
+    assert r1.contains_point(5, 5)
+    assert r1.contains_point(34, 34)
+    assert r1.contains_point(35, 35) is False
+
+
+def test_extend():
+    r1 = Rect(5, 5, 10, 10)
+    p_out = (Point(3, 3), Point(15, 5), Point(5, 15), Point(15, 15), Point(0, 0), Point(23, 34), Point(23, 2), Point(20, 34))
+    p_still_out = (Point(-1, 3), Point(15, -1), Point(24, 15), Point(20, 35))
+    p_in = (Point(5, 5), Point(6, 5), Point(14, 5), Point(5, 14), Point(14, 14))
+    for p in p_in:
+        assert r1.contains_point(*p), "{p} should be inside {r1}"
+    for p in chain(p_out, p_still_out):
+        assert not r1.contains_point(*p), "{p} should not be inside {r1}"
+    for p in p_out:
+        r1.extend(*p)
+    for p in p_in:
+        assert r1.contains_point(*p), "{p} should be inside {r1}"
+    for p in p_out:
+        assert r1.contains_point(*p), "{p} should now be inside {r1}"
+    for p in p_still_out:
+        assert not r1.contains_point(*p), "{p} should still not be inside {r1}"
 
 
 def test_rect_intersection():
