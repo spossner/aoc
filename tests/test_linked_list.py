229fe5b1f214ce9d3aaca2774a4eb967949b8e95
spossner <github@possner.de>
2022-01-06 11:18:11 +0100

    remove LinkedList class.. makes no sense.. directly work with ListNode or SinglyListNode

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,4 +1,4 @@
-from aoc.linked_list import LinkedList, SinglyListNode, ListNode
+from aoc.linked_list import SinglyListNode, ListNode
 
 def check_list(node, l):
     for expected in l:
@@ -16,39 +16,6 @@ def test_reverse_singly():
     node = SinglyListNode.from_list(reversed(l))
     new_head = node.reverse()
     check_list(new_head, l)
-
-def test_simple():
-    l = LinkedList()
-    l.addAtIndex(-1,0)
-    l.get(0)
-    l.deleteAtIndex(-1)
-    assert str(l) == "[0]"
-    assert l.validate() is True
-
-
-def test_example():
-    l = LinkedList()
-    assert l.get(0) == -1
-    assert l.get(1) == -1
-    assert l.get(2) == -1
-    l.addAtHead(1)
-    assert l.get(0) == 1
-    assert l.get(1) == -1
-    assert l.get(2) == -1
-    l.addAtTail(3)
-    assert l.get(0) == 1
-    assert l.get(1) == 3
-    assert l.get(2) == -1
-    l.addAtIndex(1, 2)
-    assert l.validate() is True
-    assert l.get(0) == 1
-    assert l.get(1) == 2
-    assert l.get(2) == 3
-    l.addAtTail(4)
-    assert l.get(2) == 3
-    l.deleteAtIndex(1)
-    assert l.get(1) == 3
-    assert l.validate() is True
 
 def test_iterator():
     l = [-4, -1, 0, 3, 10]
