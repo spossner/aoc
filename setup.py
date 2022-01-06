c70a2f70cf2fd248aacd6228c6754fd2a8ae66f4
spossner <github@possner.de>
2022-01-06 11:22:13 +0100

    next version 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aoc-lib", # Replace with your own username
-    version="1.1.1",
+    version="1.1.2",
     author="Seppo",
     author_email="aoc@possner.de",
     description="Some helper functions for advent of code puzzles",
