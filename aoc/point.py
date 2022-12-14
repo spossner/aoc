84b61b0b57aa2261c8f0c83661f2baa2b95dec25
spossner <github@possner.de>
2022-12-14 23:04:17 +0100

    add iterator from one point to another and also adapted the adjacent tests to the new ordering

--- aoc/point.py
+++ aoc/point.py
@@ -4,13 +4,18 @@ from typing import Union
 Point = namedtuple('Point', 'x,y', defaults=[0, 0])
 Point3d = namedtuple('Point3d', 'x,y,z', defaults=[0, 0, 0])
 
-DIRECT_ADJACENTS = ((0, -1), (-1, 0), (1, 0), (0, 1))  # 4 adjacent nodes
-ALL_ADJACENTS = ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1))
-
 NORTH = Point(0, -1)
 EAST = Point(1, 0)
 SOUTH = Point(0, 1)
 WEST = Point(-1, 0)
+NORTH_WEST = Point(-1, -1)
+NORTH_EAST = Point(1, -1)
+SOUTH_WEST = Point(-1, 1)
+SOUTH_EAST = Point(1, 1)
+
+
+DIRECT_ADJACENTS = (NORTH, EAST, SOUTH, WEST)  # 4 adjacent nodes
+ALL_ADJACENTS = (NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST)
 
 
 def translate(p: Union[Point, Point3d, tuple], offset: tuple) -> Union[Point, Point3d, tuple]:
@@ -38,6 +43,12 @@ def length(p) -> int:
     return sum(map(abs,p))
 
 def manhattan_distance(p1, p2) -> int:
+    '''
+    Calculates the manhattan distance of two given tuples (may be multi dimensional). Note that both tuples are assumed to have same length.
+    :param p1: first tuple
+    :param p2: second tuple with same length than p1
+    :return: the manhattan distance of both tuples
+    '''
     assert len(p1) == len(p2)
     ans = 0
     for i in range(len(p1)):
@@ -46,14 +57,36 @@ def manhattan_distance(p1, p2) -> int:
 
 
 def all_adjacent_iter(p: tuple, width: int = 0, height: int = 0):
+    '''
+    Iterates all eight adjacent points of the given point (x,y)
+    :param p: a tuple (or Point which is a named tuple) with first two elements are x and y (in that order)
+    :param width: optional width which will not be exceeded (results will always have 0 <= x < width)
+    :param height: optional height which will not be exceeded (results will always have 0 <= y < height)
+    :return: iterator of adjacent Points
+    '''
     yield from _adjacent_iter(p, width, height)
 
 
 def direct_adjacent_iter(p: tuple, width: int = 0, height: int = 0):
+    '''
+    Iterates all four direct adjacent points of the given point (x,y) - no diagonal adjacent points
+    :param p: a tuple (or Point which is a named tuple) with first two elements are x and y (in that order)
+    :param width: optional width which will not be exceeded (results will always have 0 <= x < width)
+    :param height: optional height which will not be exceeded (results will always have 0 <= y < height)
+    :return: iterator of direct adjacent Points
+    '''
     yield from _adjacent_iter(p, width, height, DIRECT_ADJACENTS)
 
 
 def _adjacent_iter(p: tuple, width: int = 0, height: int = 0, adjacents=ALL_ADJACENTS):
+    '''
+    Iterates the given adjacent [(dx,dy),...] points of the given point p (x,y)
+    :param p: a tuple (or Point which is a named tuple) with first two elements are x and y (in that order)
+    :param width: optional width which will not be exceeded (results will always have 0 <= x < width)
+    :param height: optional height which will not be exceeded (results will always have 0 <= y < height)
+    :param adjacents: the list of adjacent distances - [(dx,dy),...]
+    :return: iterator of direct adjacent Points
+    '''
     for dx, dy in adjacents:
         np = Point(p[0] + dx, p[1] + dy)
         if width > 0 and (np.x < 0 or np.x >= width):
@@ -67,3 +100,17 @@ def point_by_row(self, other):
         return self[0]-other[0]
     else:
         return self[1]-other[1]
+
+def iter_from_to(start: tuple, dest: tuple):
+    assert len(start) == len(dest)
+    pos = start
+    yield pos
+    dx = dest.x - start.x
+    dy = dest.y - start.y
+    steps = max(abs(dx), abs(dy))
+    if steps == 0:
+        return
+    offset = (dx / steps, dy / steps)
+    for i in range(steps):
+        pos = translate(pos, offset)
+        yield Point(int(pos.x), int(pos.y))
--- tests/test_point.py
+++ tests/test_point.py
@@ -1,22 +1,33 @@
 from aoc import *
 
+
 def test_rotation_cw():
     assert rot_cw(Point(0, 1)) == (-1, 0)
+
 
 def test_rotation_ccw():
     assert rot_ccw(Point(0, 1)) == (1, 0)
     assert rot_ccw(Point(1, 0)) == (0, -1)
+
 
 def test_length():
     assert length((1, 2, -3)) == 6
     assert length(Point3d(1, 2, -3)) == 6
     assert length(Point(-2, -4)) == 6
 
+
 def test_manhattan_distance():
-    assert manhattan_distance((1, 2, -3), (2,0,-3)) == 3
-    assert manhattan_distance(Point3d(1, 2, -3), Point3d(-1,-1,-1)) == 7
-    assert manhattan_distance(Point(-2, -4), Point(0,-1)) == 5
+    assert manhattan_distance((1, 2, -3), (2, 0, -3)) == 3
+    assert manhattan_distance(Point3d(1, 2, -3), Point3d(-1, -1, -1)) == 7
+    assert manhattan_distance(Point(-2, -4), Point(0, -1)) == 5
+
 
 def test_adjacents():
-    assert list(all_adjacent_iter(Point(12,5))) == [(11, 4), (12, 4), (13, 4), (11, 5), (13, 5), (11, 6), (12, 6), (13, 6)]
-    assert list(direct_adjacent_iter(Point(12, 5))) == [(12, 4), (11, 5), (13, 5), (12, 6),]
+    assert len(set(all_adjacent_iter(Point(12, 5))).difference(set([(11, 4), (12, 4), (13, 4), (11, 5), (13, 5), (11, 6), (12, 6), (13, 6)]))) == 0
+    assert len(set(direct_adjacent_iter(Point(12, 5))).difference([(12, 4), (11, 5), (13, 5), (12, 6), ])) == 0
+
+
+def test_iter_from_to():
+    assert list(iter_from_to(Point(300, 5), Point(304, 5))) == [(300, 5), (301, 5), (302, 5), (303, 5), (304, 5), ]
+    assert list(iter_from_to(Point(300, 8), Point(300, 5))) == [(300, 8), (300, 7), (300, 6), (300, 5)]
+    assert list(iter_from_to(Point(305, 5), Point(300, 0))) == [(305, 5), (304, 4), (303, 3), (302, 2), (301, 1), (300, 0)]
